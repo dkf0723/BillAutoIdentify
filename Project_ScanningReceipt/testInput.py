d7315764c7b90a6398264357e2940f93baf34ed7
Herbie <10946018@ntub.edu.tw>
2023-04-29 16:22:30 +0800

    Project ScanningReceipt

---
+++ Project_ScanningReceipt/testInput.py
@@ -0,0 +1,19 @@
+# 將多筆數字輸入list
+#list1 = list(map(int,input().split()))
+#print(list1)
+
+#正值國
+#a=input()
+#if "-" in a:
+#    print("not ok")
+#    print(a[1:len(a)])
+#else:
+#    print(a)
+
+#乘乘乘
+a = input()
+x = int(1)
+y = [int(i) for i in a]
+x = x * y
+print(y)
+print(x)
