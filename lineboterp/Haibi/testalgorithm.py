2e4142917b2ad07744d73dfac075f66ac0212bf1
Herbie <10946018@ntub.edu.tw>
2023-11-10 17:06:15 +0800

    報表管理-測試演算法
    取商品排名，因為圓餅圖合適扇面為五, 所以只取前四的值，並將剩餘的加總

--- lineboterp/Haibi/testalgorithm.py
+++ lineboterp/Haibi/testalgorithm.py
@@ -1,6 +1,14 @@
 #測試演算法
 arr = [3,7,1,84,34,96,5,57]
 arr_name = ['b','d','a','g','e','h','c','f'] #讓名字也一起換
+arr_len = len(arr)-4
+def bubbleSort(arr, arr_name):
+    for i in range(1, len(arr)):
+        for j in range(0, len(arr)-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                arr_name[j], arr_name[j + 1] = arr_name[j + 1], arr_name[j]
+    return arr, arr_name
 
 def buildMaxHeap(arr, arr_name):
     import math
@@ -31,8 +39,26 @@ def heapSort(arr, arr_name):
         arrLen -=1
         heapify(arr, arr_name, 0)
     return arr, arr_name
+print('原始數據')    
 print(arr)
 print(arr_name)
-
+# print('bubbleSort')
+# print(bubbleSort(arr))
 print('heapSort')
-print(heapSort(arr, arr_name))
+heapsort_result = heapSort(arr, arr_name)
+
+heapsort_result_name = heapsort_result[1]
+heapsort_result_figure = heapsort_result[0]
+print(heapsort_result_name)
+print(heapsort_result_figure)
+
+heapsort_result_name_limited = heapsort_result_name[arr_len:] #取前四的商品
+heapsort_result_figure_limited = heapsort_result_figure[arr_len:] #取前四商品的成本
+remaining_total_cost = 0 #取剩餘的成本
+for i in range(arr_len):
+  remaining_total_cost +=  int(heapsort_result_figure_limited[i])
+
+print('取排名')
+print(heapsort_result_name_limited)
+print(heapsort_result_figure_limited)
+print(remaining_total_cost)
