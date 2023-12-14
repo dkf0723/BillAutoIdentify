48676ef79f5bfc5092f6600c7a01321975636700
LIN-JUI-YUAN-TW <10946006@ntub.edu.tw>
2023-12-14 21:51:21 +0800

    ngrok-消費者變數宣告修正

--- lineboterp/lineboterpngrok/lineboterp_consumer/info.py
+++ lineboterp/lineboterpngrok/lineboterp_consumer/info.py
@@ -1,21 +1,12 @@
-global user_state
 user_state = {}
-global member
 member = {}
-global product
 product = {}
-global list_page
 list_page = {}
-global product_order_preorder
 product_order_preorder = {}
-global duplicate_save
 duplicate_save = {}
-global storage
 storage = {}
-global orderall
 orderall = {}
-global db
 db = {}
-global msgtype
-global user_id
-global msg
+msgtype = ''
+user_id = ''
+msg = ''
